53f0be79fef7f1e117f4e6999bbbd4e073139584
Artem Borbush <borbusartem@gmail.com>
2023-01-31 20:42:52 +0300

    first js step

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let nummberOfFilms;
+nummberOfFilms = prompt('Сколько фильмов Вы уже смотрели?', '');
+const PersonalMovieDB= {
+    'count': nummberOfFilms,
+    'movies': {},
+    'actors': {},
+    'genres': [],
+    'privat': false
+};
+
+nummberOfFilms1 = prompt('Один из последних просмотренных фильмов', '');
+nummberOfFilms2 = prompt('Насколько оцените', '');
+nummberOfFilms3 = prompt('Один из последних просмотренных фильмов', '');
+nummberOfFilms4 = prompt('Насколько оцените', '');
+const movies= {};
+movies[nummberOfFilms1]= nummberOfFilms2;
+movies[nummberOfFilms3]= nummberOfFilms4;
+console.log(PersonalMovieDB)
+console.log(movies)
